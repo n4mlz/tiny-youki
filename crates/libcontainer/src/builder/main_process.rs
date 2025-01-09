fd7221c4287370e91425afb4796ba0a5db7d2194
n4mlz <n4mlz.dev@gmail.com>
2025-01-09 20:54:48 +0900

    :sparkles: uid と gid のマッピング

--- crates/libcontainer/src/builder/main_process.rs
+++ crates/libcontainer/src/builder/main_process.rs
@@ -1,6 +1,6 @@
 use crate::*;
 use nix::unistd;
-use std::io::Result;
+use std::{io::Result, process::Command};
 
 impl ContainerBuilder {
     pub fn main_process(&self, socket: UnixSocketConnection, child_pid: unistd::Pid) -> Result<()> {
@@ -9,7 +9,51 @@ impl ContainerBuilder {
             panic!("Expected 'ready', got '{}'", message);
         }
 
-        socket.send("Hello, world!")?;
+        let linux = self.config.linux().as_ref();
+
+        if linux.is_none() {
+            return Ok(());
+        }
+
+        if let Some(uid_mappings) = linux.unwrap().uid_mappings() {
+            let mut uid_mapping_args = vec![child_pid.to_string()];
+
+            for m in uid_mappings.iter() {
+                uid_mapping_args.push(m.container_id().to_string());
+                uid_mapping_args.push(m.host_id().to_string());
+                uid_mapping_args.push(m.size().to_string());
+            }
+
+            let output = Command::new(NEWUIDMAP).args(&uid_mapping_args).output()?;
+
+            if !output.status.success() {
+                panic!(
+                    "Failed to set uid mappings: {}",
+                    String::from_utf8_lossy(&output.stderr)
+                );
+            }
+        }
+
+        if let Some(gid_mappings) = linux.unwrap().gid_mappings() {
+            let mut gid_mapping_args = vec![child_pid.to_string()];
+
+            for m in gid_mappings.iter() {
+                gid_mapping_args.push(m.container_id().to_string());
+                gid_mapping_args.push(m.host_id().to_string());
+                gid_mapping_args.push(m.size().to_string());
+            }
+
+            let output = Command::new(NEWGIDMAP).args(&gid_mapping_args).output()?;
+
+            if !output.status.success() {
+                panic!(
+                    "Failed to set gid mappings: {}",
+                    String::from_utf8_lossy(&output.stderr)
+                );
+            }
+        }
+
+        socket.send("done")?;
 
         Ok(())
     }

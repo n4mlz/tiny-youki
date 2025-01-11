12cb1d66405fa0fdb9524fe5d346997343cd579e
n4mlz <n4mlz.dev@gmail.com>
2025-01-11 11:12:17 +0900

    :recycle: namespaces.rs を別モジュールに分離

--- crates/libcontainer/src/builder.rs
+++ crates/libcontainer/src/builder.rs
@@ -3,7 +3,5 @@ mod builder;
 mod init_process;
 mod intermediate_process;
 mod main_process;
-mod namespaces;
 
 pub use builder::*;
-pub use namespaces::*;
--- crates/libcontainer/src/lib.rs
+++ crates/libcontainer/src/lib.rs
@@ -1,9 +1,11 @@
 mod builder;
 mod constant;
 mod container;
+mod namespaces;
 mod socket;
 
 pub use builder::*;
 pub use constant::*;
 pub use container::*;
+pub use namespaces::*;
 pub use socket::*;

c97e15e59d9479f3cb523577ea9295def969ea4b
n4mlz <n4mlz.dev@gmail.com>
2025-01-16 01:39:38 +0900

    :zap: 目的のプロセスを実行する処理を追加

--- crates/libcontainer/src/builder/init_process.rs
+++ crates/libcontainer/src/builder/init_process.rs
@@ -1,6 +1,12 @@
 use crate::*;
+use nix::unistd::{setgid, setuid};
 use oci_spec::runtime::LinuxNamespaceType;
-use std::io::Result;
+use std::{
+    ffi::OsString,
+    io::{Error, ErrorKind, Result},
+    os::unix::process::CommandExt,
+    process::{Command, Stdio},
+};
 
 impl ContainerBuilder {
     pub fn init_process(&self, socket: UnixSocketConnection) -> Result<()> {
@@ -25,7 +31,42 @@ impl ContainerBuilder {
             self.config.mounts().as_ref().unwrap().to_vec(),
         );
 
-        mounter.setup_rootfs()?;
+        mounter.setup()?;
+
+        if self.config.process().is_none() {
+            return Err(Error::new(ErrorKind::InvalidInput, "no process defined"));
+        }
+        let process = self.config.process().as_ref().unwrap();
+
+        setuid(process.user().uid().into())?;
+        setgid(process.user().gid().into())?;
+
+        let process_args = process.args().as_ref().unwrap();
+        let mut cmd = Command::new(&process_args[0]);
+
+        let envs = process
+            .env()
+            .as_deref()
+            .unwrap_or_default()
+            .iter()
+            .filter_map(|entry| {
+                let mut split = entry.splitn(2, '=');
+                if let (Some(key), Some(value)) = (split.next(), split.next()) {
+                    Some((OsString::from(key), OsString::from(value)))
+                } else {
+                    None
+                }
+            });
+
+        cmd.args(&process_args[1..])
+            .envs(envs)
+            .current_dir(process.cwd())
+            .stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit());
+
+        // TODO: wait for start signal
+        cmd.exec();
 
         socket.send("created")?;
 
